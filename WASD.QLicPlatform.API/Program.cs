8143d6c4d80359fa85f643c5fbaf2d299bc007e6
Pedro Guia <u202212010@upc.edu.pe>
2025-11-05 12:12:57 -0500

    chore: initial commit.

--- WASD.QLicPlatform.API/Program.cs
+++ WASD.QLicPlatform.API/Program.cs
@@ -1,17 +1,97 @@
+using WASD.QLicPlatform.API.Shared.Domain.Repositories;
+using WASD.QLicPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using WASD.QLicPlatform.API.Shared.Infrastructure.Mediator.Cortex.Configuration;
+using WASD.QLicPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using WASD.QLicPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Cortex.Mediator.Commands;
+using Cortex.Mediator.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (connectionString == null) throw new InvalidOperationException("Connection string not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    else if (builder.Environment.IsProduction())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Error);
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    
+    options.SwaggerDoc("v1",
+        new OpenApiInfo()
+        {
+            Title = "WASD.QLicPlatform.API",
+            Version = "v1",
+            Description = "WASD QLic Platform API",
+            TermsOfService = new Uri("https://wasd-qlic.com/tos"),
+            Contact = new OpenApiContact
+            {
+                Name = "WASD Corporation",
+                Email = "contact@wasd.com"
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Apache 2.0",
+                Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+            }
+        });
+    options.EnableAnnotations();
+});
+
+// Dependency Injection
+
+// Shared Bounded Context 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Mediator Configuration
+
+// Add Mediator Injection Configuration
+builder.Services.AddScoped(typeof(ICommandPipelineBehavior<>), typeof(LoggingCommandBehavior<>));
+
+// Add Cortex Mediator for Event Handling
+builder.Services.AddCortexMediator(
+    configuration: builder.Configuration,
+    handlerAssemblyMarkerTypes: [typeof(Program)], configure: options =>
+    {
+        options.AddOpenCommandPipelineBehavior(typeof(LoggingCommandBehavior<>));
+        //options.AddDefaultBehaviors();
+    });
+
 var app = builder.Build();
+
+// Verify if the database exists and create it if it doesn't
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
