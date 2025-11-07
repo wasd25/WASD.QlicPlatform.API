c9b87491d3c40cee0a76f19101fd6796cd2338d5
Pedro Guia <u202212010@upc.edu.pe>
2025-11-07 15:02:17 -0500

    feat(alerts): add command and query services.

---
+++ WASD.QLicPlatform.API/Alerts/Services/IAlertCommandService.cs
@@ -0,0 +1,11 @@
+using WASD.QLicPlatform.API.Alerts.Domain.Model.Aggregate;
+using WASD.QLicPlatform.API.Alerts.Domain.Model.Commands;
+
+namespace WASD.QLicPlatform.API.Alerts.Services;
+
+public interface IAlertCommandService
+{
+    public Task<Alert?> Handle(CreateAlertCommand command);
+    public Task<Alert?> Handle(DeleteAlertCommand command);
+    public Task<Alert?> Handle(UpdateAlertCommand command);
+}
