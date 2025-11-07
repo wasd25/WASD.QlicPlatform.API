1f3679ef62330df471cefc027490bd0be426910e
Pedro Guia <u202212010@upc.edu.pe>
2025-11-07 14:17:06 -0500

    feat(alerts): add alert type enum.

---
+++ WASD.QLicPlatform.API/Alerts/Domain/Model/ValueObjects/EAlertType.cs
@@ -0,0 +1,11 @@
+namespace WASD.QLicPlatform.API.Alerts.Domain.Model.ValueObjects;
+
+/// <summary>
+///  Represents the type of alert in the WASD QLic Platform.
+/// </summary>
+public enum EAlertType
+{
+    Critical,
+    Warning,
+    Info
+}
