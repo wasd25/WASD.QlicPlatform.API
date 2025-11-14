dbec4ead7b2a5f033263eafd990935a818cd15bb
Jafeth <jafethworren@gmail.com>
2025-11-14 16:15:47 -0500

    feat: add UsageEventCommandService for registering usage events

---
+++ WASD.QLicPlatform.API/UsageManagement/Application/Internal/CommandServices/UsageEventCommandService.cs
@@ -0,0 +1,21 @@
+﻿using WASD.QLicPlatform.API.UsageManagement.Domain.Models;
+using WASD.QLicPlatform.API.UsageManagement.Domain.Repositories;
+
+namespace WASD.QLicPlatform.API.UsageManagement.Application.Internal.CommandServices;
+
+public class UsageEventCommandService
+{
+    private readonly IUsageEventRepository _repository;
+
+    public UsageEventCommandService(IUsageEventRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<UsageEvent> RegisterAsync(DateTime time, int amount, string source)
+    {
+        var usageEvent = new UsageEvent(time, amount, source);
+        await _repository.AddAsync(usageEvent);
+        return usageEvent;
+    }
+}
