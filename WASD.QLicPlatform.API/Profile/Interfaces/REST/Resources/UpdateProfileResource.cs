8fb16ac2ad21111ca8a68484002ddfe1ad7b1ac7
Ayrton <u202311077@upc.edu.pe>
2025-11-14 08:19:45 -0500

    feat(Profile): add profile resource

---
+++ WASD.QLicPlatform.API/Profile/Interfaces/REST/Resources/UpdateProfileResource.cs
@@ -0,0 +1,9 @@
+﻿namespace WASD.QLicPlatform.API.Profile.Interfaces.REST.Resources;
+
+public record UpdateProfileResource(
+    string FirstName,
+    string LastName,
+    int? Age,
+    string? Phone,
+    string? Address
+);
