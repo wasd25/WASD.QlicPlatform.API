f3e7eef85b33963dbe2a620d6b6e236f2de9fea8
lowmath <u202215004@upc.edu.pe>
2025-11-14 15:13:55 -0500

    feat: add Subscription model with properties and constructor

---
+++ WASD.QLicPlatform.API/Subscriptions/Domain/Model/Aggregate/Subscription.cs
@@ -0,0 +1,17 @@
+﻿namespace WASD.QLicPlatform.API.Subscriptions.Domain.Model.Aggregate;
+
+public class Subscription
+{
+    public int Id { get; }
+    public string Plan { get; }
+    public string Price { get; }
+    public string Description { get; }
+
+    public Subscription(int id, string plan, string price, string description)
+    {
+        Id = id;
+        Plan = plan;
+        Price = price;
+        Description = description;
+    }
+}
