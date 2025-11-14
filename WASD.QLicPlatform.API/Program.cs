f2457bb55719700e1a71bf48c470764d97b9465c
Jafeth <jafethworren@gmail.com>
2025-11-14 16:15:36 -0500

    feat: register UsageManagement services and repositories in Program.cs

--- WASD.QLicPlatform.API/Program.cs
+++ WASD.QLicPlatform.API/Program.cs
@@ -8,13 +8,22 @@ using Cortex.Mediator.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
+// UsageManagement imports
+using WASD.QLicPlatform.API.UsageManagement.Domain.Repositories;
+using WASD.QLicPlatform.API.UsageManagement.Infrastructure.Persistence.EFC.Repositories;
+using WASD.QLicPlatform.API.UsageManagement.Application.Internal.CommandServices;
+using WASD.QLicPlatform.API.UsageManagement.Application.Internal.QueryServices;
+using WASD.QLicPlatform.API.UsageManagement.Domain.Services;
+using WASD.QLicPlatform.API.UsageManagement.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// AutoMapper Configuration
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (connectionString == null) throw new InvalidOperationException("Connection string not found.");
@@ -31,11 +40,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
             .LogTo(Console.WriteLine, LogLevel.Error);
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    
     options.SwaggerDoc("v1",
         new OpenApiInfo()
         {
@@ -62,12 +70,22 @@ builder.Services.AddSwaggerGen(options =>
 // Shared Bounded Context 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-// Mediator Configuration
+// UsageManagement Repositories
+builder.Services.AddScoped<IUsageSummaryRepository, UsageSummaryRepository>();
+builder.Services.AddScoped<IUsageEventRepository, UsageEventRepository>();
 
-// Add Mediator Injection Configuration
+// UsageManagement Services
+builder.Services.AddScoped<UsageSummaryCommandService>();
+builder.Services.AddScoped<UsageSummaryQueryService>();
+builder.Services.AddScoped<UsageEventCommandService>();
+builder.Services.AddScoped<UsageEventQueryService>();
+
+// UsageManagement Domain Services
+builder.Services.AddScoped<IUsageAnalysisService, UsageAnalysisService>();
+
+// Mediator Configuration
 builder.Services.AddScoped(typeof(ICommandPipelineBehavior<>), typeof(LoggingCommandBehavior<>));
 
-// Add Cortex Mediator for Event Handling
 builder.Services.AddCortexMediator(
     configuration: builder.Configuration,
     handlerAssemblyMarkerTypes: [typeof(Program)], configure: options =>
