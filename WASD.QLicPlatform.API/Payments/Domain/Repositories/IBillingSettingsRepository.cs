e3f0d3d1939000d6ca083349b9593963d9ec886c
lowmath <u202215004@upc.edu.pe>
2025-11-14 09:26:24 -0500

    feat: add interfaces for billing settings command and query services

---
+++ WASD.QLicPlatform.API/Payments/Domain/Repositories/IBillingSettingsRepository.cs
@@ -0,0 +1,10 @@
+﻿using WASD.QLicPlatform.API.Payments.Domain.Model.Aggregate;
+using WASD.QLicPlatform.API.Shared.Domain.Repositories;
+
+namespace WASD.QLicPlatform.API.Payments.Domain.Repositories;
+
+public interface IBillingSettingsRepository : IBaseRepository<BillingSettings>
+{
+    Task<bool> AnyAsync();
+    Task<BillingSettings?> GetSingletonAsync();
+}
