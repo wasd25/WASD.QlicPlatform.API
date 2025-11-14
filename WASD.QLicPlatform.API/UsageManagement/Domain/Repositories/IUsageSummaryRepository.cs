632e0206da944a0f67e93e2ce4d48562c3246daa
Jafeth <jafethworren@gmail.com>
2025-11-14 16:15:30 -0500

    feat: add IUsageSummaryRepository interface for managing usage summaries

---
+++ WASD.QLicPlatform.API/UsageManagement/Domain/Repositories/IUsageSummaryRepository.cs
@@ -0,0 +1,11 @@
+﻿using WASD.QLicPlatform.API.UsageManagement.Domain.Models;
+
+namespace WASD.QLicPlatform.API.UsageManagement.Domain.Repositories;
+
+public interface IUsageSummaryRepository
+{
+    Task<UsageSummary?> GetAsync();
+    Task AddAsync(UsageSummary summary);
+    Task UpdateAsync(UsageSummary summary);
+    Task DeleteAsync(Guid id);
+}
