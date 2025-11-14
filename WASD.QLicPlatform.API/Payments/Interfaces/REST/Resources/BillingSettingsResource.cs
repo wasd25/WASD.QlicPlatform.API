45901cb5f6ed4a0c1deb4f9d301a24567c8e572b
lowmath <u202215004@upc.edu.pe>
2025-11-14 09:26:50 -0500

    feat: add BillingSettingsController and related resources for managing billing settings

---
+++ WASD.QLicPlatform.API/Payments/Interfaces/REST/Resources/BillingSettingsResource.cs
@@ -0,0 +1,9 @@
+﻿namespace WASD.QLicPlatform.API.Payments.Interfaces.REST.Resources;
+
+public record BillingSettingsResource(
+    int Id,
+    bool Autopay,
+    bool EmailNotifications,
+    string BillingCycle,
+    int PreferredBillingDay,
+    System.DateTime LastUpdate);
