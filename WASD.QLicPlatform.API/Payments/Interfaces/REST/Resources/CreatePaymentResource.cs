eed3eb9dbef96b83f8c029c1257769d078235d1c
lowmath <u202215004@upc.edu.pe>
2025-11-14 09:33:08 -0500

    feat: add CreatePaymentResource and DeletePaymentResource for payment operations

---
+++ WASD.QLicPlatform.API/Payments/Interfaces/REST/Resources/CreatePaymentResource.cs
@@ -0,0 +1,3 @@
+﻿namespace WASD.QLicPlatform.API.Payments.Interfaces.REST.Resources;
+
+public record CreatePaymentResource(int Id, decimal Amount, System.DateTime Date, string Status, string MethodId);
