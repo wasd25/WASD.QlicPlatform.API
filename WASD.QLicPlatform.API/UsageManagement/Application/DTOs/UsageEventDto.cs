880251cf7f1650241bd7d759de381bbd6b23ec1e
Jafeth <jafethworren@gmail.com>
2025-11-14 16:15:55 -0500

    feat: add UsageEventDto for data transfer of usage events

---
+++ WASD.QLicPlatform.API/UsageManagement/Application/DTOs/UsageEventDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WASD.QLicPlatform.API.UsageManagement.Application.DTOs;
+
+public class UsageEventDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string Time { get; set; } = string.Empty; // formato HH:mm
+    public int Amount { get; set; }
+    public string Source { get; set; } = string.Empty;
+}
