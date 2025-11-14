ea86eb70463e332da0bdbb84a33336ff39c13af3
Jafeth <jafethworren@gmail.com>
2025-11-14 12:03:19 -0500

    depure

--- WASD.QLicPlatform.API/Alerts/Interfaces/REST/Resources/UpdateAlertResource.cs
+++
@@ -1,3 +0,0 @@
-namespace WASD.QLicPlatform.API.Alerts.Interfaces.REST.Resources;
-
-public record UpdateAlertResource(int AlertId, string AlertType, string Title, string Message, string Timestamp);
--- WASD.QLicPlatform.API/Program.cs
+++ WASD.QLicPlatform.API/Program.cs
@@ -13,11 +13,6 @@ using WASD.QLicPlatform.API.IAM.Infrastructure.Persistence.Repositories;
 using WASD.QLicPlatform.API.IAM.Infrastructure.Services;
 using WASD.QLicPlatform.API.Profile.Domain.Repositories;
 using WASD.QLicPlatform.API.Profile.Infrastructure.Persistence.Repositories;
-using WASD.QLicPlatform.API.Alerts.Application.Internal.CommandServices;
-using WASD.QLicPlatform.API.Alerts.Application.Internal.QueryServices;
-using WASD.QLicPlatform.API.Alerts.Domain.Repositories;
-using WASD.QLicPlatform.API.Alerts.Domain.Services;
-using WASD.QLicPlatform.API.Alerts.Infrastructure.Persistence.EFC.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -80,11 +75,6 @@ builder.Services.AddScoped<ITokenService, JwtTokenService>();
 
 // Shared Bounded Context 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-// Alert Bounded Context
-builder.Services.AddScoped<IAlertRepository, AlertRepository>();
-builder.Services.AddScoped<IAlertCommandService, AlertCommandService>();
-builder.Services.AddScoped<IAlertQueryService, AlertQueryService>();
 
 // Mediator Configuration
 
