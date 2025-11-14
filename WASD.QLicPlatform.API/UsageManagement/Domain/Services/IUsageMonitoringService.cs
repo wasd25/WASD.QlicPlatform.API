c50fe3a6e46720209c280cb9886be9977f63084a
Jafeth <jafethworren@gmail.com>
2025-11-14 16:15:26 -0500

    feat: add IUsageMonitoringService interface for usage limit checks

---
+++ WASD.QLicPlatform.API/UsageManagement/Domain/Services/IUsageMonitoringService.cs
@@ -0,0 +1,7 @@
+﻿namespace WASD.QLicPlatform.API.UsageManagement.Domain.Services;
+
+public interface IUsageMonitoringService
+{
+    bool HasExceededDailyLimit(int current, int dailyLimit);
+    bool HasExceededMonthlyTotal(int monthlyTotal, int threshold);
+}
