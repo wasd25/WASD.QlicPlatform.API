f4a6622e28f81732cc8a4c4c305389b6b401fbc3
lowmath <u202215004@upc.edu.pe>
2025-11-14 09:33:25 -0500

    feat: add IPaymentCommandService, IPaymentQueryService, and IPaymentRepository interfaces for payment operations

---
+++ WASD.QLicPlatform.API/Payments/Domain/Repositories/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+﻿using WASD.QLicPlatform.API.Payments.Domain.Model.Aggregate;
+using WASD.QLicPlatform.API.Shared.Domain.Repositories;
+
+namespace WASD.QLicPlatform.API.Payments.Domain.Repositories;
+
+public interface IPaymentRepository : IBaseRepository<Payment>
+{
+    // añadir métodos específicos si se requieren (p.ej. FindByMethodIdAsync)
+}
