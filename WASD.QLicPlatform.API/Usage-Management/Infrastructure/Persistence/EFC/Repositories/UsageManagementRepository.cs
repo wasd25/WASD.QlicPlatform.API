4311180f10e95e3defab4931cf335e2dc571f411
Pedro Guia <u202212010@upc.edu.pe>
2025-11-18 16:58:06 -0500

    feat(usage-management): add usage management repository.

---
+++ WASD.QLicPlatform.API/Usage-Management/Infrastructure/Persistence/EFC/Repositories/UsageManagementRepository.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using WASD.QLicPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using WASD.QLicPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using WASD.QLicPlatform.API.Usage_Management.Domain.Model.Aggregate;
+using WASD.QLicPlatform.API.Usage_Management.Domain.Repositories;
+
+namespace WASD.QLicPlatform.API.Usage_Management.Infrastructure.Persistence.EFC.Repositories;
+
+public class UsageManagementRepository(AppDbContext context)
+    : BaseRepository<UsageEvents>(context), IUsageManagementRepository
+{
+    public async Task<IEnumerable<UsageEvents>> findBySource(string source)
+    {
+        return await Context.Set<UsageEvents>().Where(u => u.Source == source).ToListAsync();
+    }
+
+    public async Task<UsageEvents?> FindByIdAsync(int id)
+    {
+        return await Context.Set<UsageEvents>().FirstOrDefaultAsync(u => u.Id == id);
+    }
+}
