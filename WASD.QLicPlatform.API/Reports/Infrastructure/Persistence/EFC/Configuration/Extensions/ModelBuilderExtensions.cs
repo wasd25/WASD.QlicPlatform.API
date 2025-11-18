abe3e346d21ab046f58bc426032008a656b346a7
Everkoe <angeloalejos15@gmail.com>
2025-11-18 17:37:52 -0500

    build: added ModelBuilderExtensions for reports configuration

---
+++ WASD.QLicPlatform.API/Reports/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WASD.QLicPlatform.API.Reports.Infrastructure.Persistence.EFC.Configuration;
+
+namespace WASD.QLicPlatform.API.Reports.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void ApplyReportsConfiguration(this ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new ReportsContextConfiguration());
+    }
+}
