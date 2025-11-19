de2fa2778a5341d6dfce898c28100a6e06222a02
Everkoe <angeloalejos15@gmail.com>
2025-11-19 09:20:26 -0500

    feat(dto): prepare UpdateReportSummaryResource for future nested updates

---
+++ WASD.QLicPlatform.API/Reports/Interfaces/REST/Resources/UpdateReportSummaryResource.cs
@@ -0,0 +1,8 @@
+﻿namespace WASD.QLicPlatform.API.Reports.Interfaces.REST.Resources;
+
+public class UpdateReportSummaryResource
+{
+    public string Type { get; set; } = null!;
+    public string Location { get; set; } = null!;
+    public string Period { get; set; } = null!;
+}
