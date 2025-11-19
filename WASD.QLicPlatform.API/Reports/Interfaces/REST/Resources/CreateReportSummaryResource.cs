acfc29b8c24dd4964dd3c6eace6454b8c4115b28
Everkoe <angeloalejos15@gmail.com>
2025-11-19 09:18:24 -0500

    feat(dto): extend CreateReportSummaryResource to include nested children

---
+++ WASD.QLicPlatform.API/Reports/Interfaces/REST/Resources/CreateReportSummaryResource.cs
@@ -0,0 +1,14 @@
+﻿namespace WASD.QLicPlatform.API.Reports.Interfaces.REST.Resources
+{
+    public class CreateReportSummaryResource
+    {
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Period { get; set; }
+        public string Resource { get; set; }
+
+        public EfficiencyMetricsDto EfficiencyMetrics { get; set; }
+        public List<UsageTrendDto> UsageTrends { get; set; }
+        public List<CostBreakdownDto> CostBreakdown { get; set; }
+    }
+}
