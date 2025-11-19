3246f65a91c2e39bd844119fa39c8d3d7c7416f2
Pedro Guia <u202212010@upc.edu.pe>
2025-11-18 21:40:19 -0500

    feat(usage-management): fix model builder by usages.

--- WASD.QLicPlatform.API/Usage-Management/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensionsUsageEvents.cs
+++ WASD.QLicPlatform.API/Usage-Management/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensionsUsageEvents.cs
@@ -3,15 +3,10 @@ using WASD.QLicPlatform.API.Usage_Management.Domain.Model.Aggregate;
 
 namespace WASD.QLicPlatform.API.Usage_Management.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
-public static class ModelBuilderExtensions
+public static class ModelBuilderExtensionsUsageEvents
 {
-    public static void ApplyUsageManagement(this ModelBuilder builder)
+    public static void ApplyUsageEvents(this ModelBuilder builder)
     {
-        // usage Summary Context
-        builder.Entity<UsageSummary>().Property(u => u.Current).IsRequired();
-        builder.Entity<UsageSummary>().Property(u => u.DailyLimit).IsRequired();
-        builder.Entity<UsageSummary>().Property(u => u.MonthlyTotal).IsRequired();
-        
         // Usage Events Context
         builder.Entity<UsageEvents>().HasKey(u => u.Id);
         builder.Entity<UsageEvents>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
---
+++ WASD.QLicPlatform.API/Usage-Management/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensionsUsageSummary.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WASD.QLicPlatform.API.Usage_Management.Domain.Model.Aggregate;
+
+namespace WASD.QLicPlatform.API.Usage_Management.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class ModelBuilderExtensionsUsageSummary
+{
+    public static void ApplyUsageSummary(this ModelBuilder builder)
+    {
+        // usage Summary Context
+        builder.Entity<UsageSummary>().Property(u => u.Current).IsRequired();
+        builder.Entity<UsageSummary>().Property(u => u.DailyLimit).IsRequired();
+        builder.Entity<UsageSummary>().Property(u => u.MonthlyTotal).IsRequired();
+    }
+}
