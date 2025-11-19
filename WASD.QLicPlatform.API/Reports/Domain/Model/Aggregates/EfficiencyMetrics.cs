297d7c3655f6ac662934006e0feed5b84ab46fe7
Everkoe <angeloalejos15@gmail.com>
2025-11-19 09:18:29 -0500

    feat(domain): define EfficiencyMetrics entity with one-to-one relation to ReportSummary

---
+++ WASD.QLicPlatform.API/Reports/Domain/Model/Aggregates/EfficiencyMetrics.cs
@@ -0,0 +1,12 @@
+﻿namespace WASD.QLicPlatform.API.Reports.Domain.Model.Aggregates;
+
+public class EfficiencyMetrics
+{
+    public long Id { get; set; }
+    public int Score { get; set; }
+    public int WaterSaved { get; set; }
+    public int CostSaved { get; set; }
+
+    public long ReportSummaryId { get; set; }
+    public ReportSummary? ReportSummary { get; set; }
+}
