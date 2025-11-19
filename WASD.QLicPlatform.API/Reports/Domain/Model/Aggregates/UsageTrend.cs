c048439c06c679ce9f6bfd68ac768e9273b443ed
Everkoe <angeloalejos15@gmail.com>
2025-11-19 09:20:32 -0500

    feat(domain): define UsageTrend entity with foreign key to ReportSummary

---
+++ WASD.QLicPlatform.API/Reports/Domain/Model/Aggregates/UsageTrend.cs
@@ -0,0 +1,11 @@
+﻿namespace WASD.QLicPlatform.API.Reports.Domain.Model.Aggregates;
+
+public class UsageTrend
+{
+    public long Id { get; set; }
+    public string Day { get; set; } = null!;
+    public int Liters { get; set; }
+
+    public long ReportSummaryId { get; set; }
+    public ReportSummary? ReportSummary { get; set; }
+}
