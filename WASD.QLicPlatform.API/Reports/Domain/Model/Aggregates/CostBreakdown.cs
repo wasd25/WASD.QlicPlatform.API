2a5ba64105bd8276596e377fe555433798e5d43e
Everkoe <angeloalejos15@gmail.com>
2025-11-19 09:17:55 -0500

    feat(domain): define CostBreakdown entity with foreign key to ReportSummary

---
+++ WASD.QLicPlatform.API/Reports/Domain/Model/Aggregates/CostBreakdown.cs
@@ -0,0 +1,11 @@
+﻿namespace WASD.QLicPlatform.API.Reports.Domain.Model.Aggregates;
+
+public class CostBreakdown
+{
+    public long Id { get; set; }
+    public string Category { get; set; } = null!;
+    public decimal Cost { get; set; }
+
+    public long ReportSummaryId { get; set; }
+    public ReportSummary? ReportSummary { get; set; }
+}
