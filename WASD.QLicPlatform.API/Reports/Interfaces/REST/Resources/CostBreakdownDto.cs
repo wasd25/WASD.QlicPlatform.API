6d4c71de4b9dc0227a03e772f6b9ad824c770be0
Everkoe <angeloalejos15@gmail.com>
2025-11-19 09:18:02 -0500

    feat(dto): add CostBreakdownDto for REST resource mapping

---
+++ WASD.QLicPlatform.API/Reports/Interfaces/REST/Resources/CostBreakdownDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WASD.QLicPlatform.API.Reports.Interfaces.REST.Resources
+{
+    public class CostBreakdownDto
+    {
+        public string Category { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
