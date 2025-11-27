d305f02fcf930101550b3d25f13d378b69fcca4a
lowmath <u202215004@upc.edu.pe>
2025-11-26 19:22:22 -0500

    feat: add commands and resources for creating payments and payment methods

---
+++ WASD.QLicPlatform.API/Payments/Interfaces/REST/Resources/CreatePaymentResource.cs
@@ -0,0 +1,8 @@
+﻿namespace WASD.QLicPlatform.API.Payments.Interfaces.REST.Resources;
+
+public record CreatePaymentResource(
+    decimal Amount,
+    DateTime Date,
+    string Status,
+    int MethodId
+);
---
+++ WASD.QLicPlatform.API/Payments/Interfaces/REST/Transform/CreatePaymentCommandFromSourceAssembler.cs
@@ -0,0 +1,17 @@
+﻿using WASD.QLicPlatform.API.Payments.Domain.Model.Commands;
+using WASD.QLicPlatform.API.Payments.Interfaces.REST.Resources;
+
+namespace WASD.QLicPlatform.API.Payments.Interfaces.REST.Transform;
+
+public static class CreatePaymentCommandFromResourceAssembler
+{
+    public static CreatePaymentCommand ToCommandFromResource(CreatePaymentResource resource)
+    {
+        return new CreatePaymentCommand(
+            resource.Amount,
+            resource.Date,
+            resource.Status,
+            resource.MethodId
+        );
+    }
+}
