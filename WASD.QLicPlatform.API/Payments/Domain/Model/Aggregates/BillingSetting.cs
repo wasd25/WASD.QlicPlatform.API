b20aff45e9d53e06af146bd42cffbacd464aa139
lowmath <u202215004@upc.edu.pe>
2025-11-26 19:19:27 -0500

    feat: add BillingSetting model for payment configuration

---
+++ WASD.QLicPlatform.API/Payments/Domain/Model/Aggregates/BillingSetting.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WASD.QLicPlatform.API.Payments.Domain.Model.Aggregates;
+
+public class BillingSetting
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    [Required]
+    public bool Autopay { get; set; }
+    
+    [Required]
+    public bool EmailNotifications { get; set; }
+    
+    [Required]
+    [MaxLength(20)]
+    public string BillingCycle { get; set; } = "monthly";
+    
+    [Required]
+    [Range(1, 31)]
+    public int PreferredBillingDay { get; set; }
+    
+    [Required]
+    public DateTime LastUpdate { get; set; }
+    
+    // User ID reference (assuming you have user management)
+    public int? UserId { get; set; }
+}
+
