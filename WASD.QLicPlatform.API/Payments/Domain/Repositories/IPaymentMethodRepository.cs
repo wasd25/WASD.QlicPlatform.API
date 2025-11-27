b8c2d306fb9b9c50f4e0a63e61190c48b4eb296e
lowmath <u202215004@upc.edu.pe>
2025-11-26 19:23:03 -0500

    feat: add repositories and query services for billing settings and payment methods

---
+++ WASD.QLicPlatform.API/Payments/Domain/Repositories/IPaymentMethodRepository.cs
@@ -0,0 +1,11 @@
+﻿using WASD.QLicPlatform.API.Payments.Domain.Model.Aggregates;
+using WASD.QLicPlatform.API.Shared.Domain.Repositories;
+
+namespace WASD.QLicPlatform.API.Payments.Domain.Repositories;
+
+public interface IPaymentMethodRepository : IBaseRepository<PaymentMethod>
+{
+    Task<PaymentMethod?> FindDefaultAsync();
+    Task<IEnumerable<PaymentMethod>> FindByTypeAsync(string type);
+}
+
