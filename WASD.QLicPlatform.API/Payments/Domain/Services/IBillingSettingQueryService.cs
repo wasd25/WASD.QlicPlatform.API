bab11cf5fc7f9b7f3b1302edfae0c4a0563502ee
lowmath <u202215004@upc.edu.pe>
2025-11-26 19:20:58 -0500

    feat: add IBillingSettingCommandService and IBillingSettingQueryService interfaces for billing settings management

---
+++ WASD.QLicPlatform.API/Payments/Domain/Services/IBillingSettingQueryService.cs
@@ -0,0 +1,10 @@
+﻿using WASD.QLicPlatform.API.Payments.Domain.Model.Aggregates;
+using WASD.QLicPlatform.API.Payments.Domain.Model.Queries;
+
+namespace WASD.QLicPlatform.API.Payments.Domain.Services;
+
+public interface IBillingSettingQueryService
+{
+    Task<BillingSetting?> Handle(GetBillingSettingQuery query);
+}
+
