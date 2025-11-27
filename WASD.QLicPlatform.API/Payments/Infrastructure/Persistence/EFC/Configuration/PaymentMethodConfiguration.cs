73c8533ccca97c8babf38d927b8dcc52754700fa
lowmath <u202215004@upc.edu.pe>
2025-11-26 19:23:28 -0500

    feat: add PaymentMethodConfiguration for configuring payment method entity

---
+++ WASD.QLicPlatform.API/Payments/Infrastructure/Persistence/EFC/Configuration/PaymentMethodConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WASD.QLicPlatform.API.Payments.Domain.Model.Aggregates;
+
+namespace WASD.QLicPlatform.API.Payments.Infrastructure.Persistence.EFC.Configuration;
+
+public class PaymentMethodConfiguration : IEntityTypeConfiguration<PaymentMethod>
+{
+    public void Configure(EntityTypeBuilder<PaymentMethod> builder)
+    {
+        builder.ToTable("payment_methods");
+        
+        builder.HasKey(pm => pm.Id);
+        
+        builder.Property(pm => pm.Id)
+            .HasColumnName("id")
+            .IsRequired()
+            .ValueGeneratedOnAdd()
+            .UseMySqlIdentityColumn();
+        
+        builder.Property(pm => pm.Type)
+            .HasColumnName("type")
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        builder.Property(pm => pm.Details)
+            .HasColumnName("details")
+            .HasMaxLength(255)
+            .IsRequired();
+        
+        builder.Property(pm => pm.IsDefault)
+            .HasColumnName("is_default")
+            .IsRequired()
+            .HasDefaultValue(false);
+    }
+}
+
