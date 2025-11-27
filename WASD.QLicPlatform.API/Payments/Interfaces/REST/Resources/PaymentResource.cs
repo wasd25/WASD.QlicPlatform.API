dcc399891ff8c17c8fd4e997bde74611d9d05222
lowmath <u202215004@upc.edu.pe>
2025-11-26 19:24:14 -0500

    feat: add PaymentMethodRepository, PaymentMethodsController, and related resources for payment method management

---
+++ WASD.QLicPlatform.API/Payments/Interfaces/REST/Resources/PaymentResource.cs
@@ -0,0 +1,9 @@
+﻿namespace WASD.QLicPlatform.API.Payments.Interfaces.REST.Resources;
+
+public record PaymentResource(
+    int Id,
+    decimal Amount,
+    DateTime Date,
+    string Status,
+    int MethodId
+);
---
+++ WASD.QLicPlatform.API/Payments/Interfaces/REST/Transform/PaymentResourceFromEntityAssembler.cs
@@ -0,0 +1,18 @@
+﻿using WASD.QLicPlatform.API.Payments.Domain.Model.Aggregate;
+using WASD.QLicPlatform.API.Payments.Interfaces.REST.Resources;
+
+namespace WASD.QLicPlatform.API.Payments.Interfaces.REST.Transform;
+
+public static class PaymentResourceFromEntityAssembler
+{
+    public static PaymentResource ToResourceFromEntity(Payment entity)
+    {
+        return new PaymentResource(
+            entity.Id,
+            entity.Amount,
+            entity.Date,
+            entity.Status,
+            entity.MethodId
+        );
+    }
+}
