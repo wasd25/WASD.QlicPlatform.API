3df918f5bb58fda5029a0c47adf81636f12070eb
lowmath <u202215004@upc.edu.pe>
2025-11-26 19:39:59 -0500

    feat: add ISubscriptionRepository interface for managing subscription data

---
+++ WASD.QLicPlatform.API/Subscriptions/Domain/Repositories/ISubscriptionRepository.cs
@@ -0,0 +1,9 @@
+﻿using WASD.QLicPlatform.API.Subscriptions.Domain.Model.Aggregates;
+using WASD.QLicPlatform.API.Shared.Domain.Repositories;
+
+namespace WASD.QLicPlatform.API.Subscriptions.Domain.Repositories;
+
+public interface ISubscriptionRepository : IBaseRepository<Subscription>
+{
+}
+
