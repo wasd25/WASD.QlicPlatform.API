4224c72b109e627b215b82049e07372bcb1e202c
lowmath <u202215004@upc.edu.pe>
2025-11-26 19:39:54 -0500

    feat: add ISubscriptionQueryService interface for handling subscription queries

---
+++ WASD.QLicPlatform.API/Subscriptions/Domain/Services/ISubscriptionQueryService.cs
@@ -0,0 +1,10 @@
+﻿using WASD.QLicPlatform.API.Subscriptions.Domain.Model.Aggregates;
+using WASD.QLicPlatform.API.Subscriptions.Domain.Model.Queries;
+
+namespace WASD.QLicPlatform.API.Subscriptions.Domain.Services;
+
+public interface ISubscriptionQueryService
+{
+    Task<Subscription?> Handle(GetSubscriptionByIdQuery query);
+}
+
