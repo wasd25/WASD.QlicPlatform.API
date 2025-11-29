8f194eaf01017134e1389a285828bc7bef12562d
lowmath <u202215004@upc.edu.pe>
2025-11-28 22:25:06 -0500

    feat: possible fix for render deploy

--- WASD.QLicPlatform.API/Program.cs
+++ WASD.QLicPlatform.API/Program.cs
@@ -244,11 +244,13 @@ using (var scope = app.Services.CreateScope())
 // -----------------------------
 // Middleware Pipeline
 // -----------------------------
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WASD.QLicPlatform.API v1");
+    c.RoutePrefix = string.Empty; 
+});
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
