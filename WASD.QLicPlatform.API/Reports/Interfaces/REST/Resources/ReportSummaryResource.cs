c7cc0ea0bdc30811e35f0477dde8dd5ecef8aee3
Everkoe <angeloalejos15@gmail.com>
2025-11-28 19:45:52 -0500

    feat(reports): add ReportSummaryResource for GET responses

--- WASD.QLicPlatform.API/Reports/Interfaces/REST/Resources/ReportSummaryResource.cs
+++ WASD.QLicPlatform.API/Reports/Interfaces/REST/Resources/ReportSummaryResource.cs
@@ -1,15 +1,15 @@
-﻿namespace WASD.QLicPlatform.API.Reports.Interfaces.REST.Resources
-{
-    public class ReportSummaryResource
-    {
-        public long Id { get; set; }
-        public string Type { get; set; }
-        public string Location { get; set; }
-        public string Period { get; set; }
-        public string Resource { get; set; }
+﻿// Interfaces/REST/Resources/ReportSummaryResource.cs
+namespace WASD.QLicPlatform.API.Reports.Interfaces.REST.Resources;
 
-        public EfficiencyMetricsDto EfficiencyMetrics { get; set; }
-        public List<UsageTrendDto> UsageTrends { get; set; }
-        public List<CostBreakdownDto> CostBreakdown { get; set; }
-    }
+public class ReportSummaryResource
+{
+    public long Id { get; set; }
+    public string Type { get; set; } = default!;
+    public string Location { get; set; } = default!;
+    public string Period { get; set; } = default!;
+    public string Resource { get; set; } = default!;
+
+    public EfficiencyMetricsDto? EfficiencyMetrics { get; set; }
+    public List<UsageTrendDto>? UsageTrends { get; set; }
+    public List<CostBreakdownDto>? CostBreakdown { get; set; }
 }
