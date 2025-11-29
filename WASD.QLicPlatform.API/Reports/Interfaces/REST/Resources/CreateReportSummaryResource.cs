55e585684bd618f8b14bfe6b25c13480cbbc897e
Everkoe <angeloalejos15@gmail.com>
2025-11-28 19:45:24 -0500

    feat(reports): add CreateReportSummaryResource for POST requests

--- WASD.QLicPlatform.API/Reports/Interfaces/REST/Resources/CreateReportSummaryResource.cs
+++ WASD.QLicPlatform.API/Reports/Interfaces/REST/Resources/CreateReportSummaryResource.cs
@@ -1,14 +1,16 @@
-﻿namespace WASD.QLicPlatform.API.Reports.Interfaces.REST.Resources
-{
-    public class CreateReportSummaryResource
-    {
-        public string Type { get; set; }
-        public string Location { get; set; }
-        public string Period { get; set; }
-        public string Resource { get; set; }
+﻿// Interfaces/REST/Resources/CreateReportSummaryResource.cs
+using System.ComponentModel.DataAnnotations;
 
-        public EfficiencyMetricsDto EfficiencyMetrics { get; set; }
-        public List<UsageTrendDto> UsageTrends { get; set; }
-        public List<CostBreakdownDto> CostBreakdown { get; set; }
-    }
+namespace WASD.QLicPlatform.API.Reports.Interfaces.REST.Resources;
+
+public class CreateReportSummaryResource
+{
+    [Required] public string Type { get; set; } = default!;
+    [Required] public string Location { get; set; } = default!;
+    [Required] public string Period { get; set; } = default!;
+    [Required] public string Resource { get; set; } = default!;  // <-- nombre exacto
+
+    public EfficiencyMetricsDto? EfficiencyMetrics { get; set; }
+    public List<UsageTrendDto>? UsageTrends { get; set; }
+    public List<CostBreakdownDto>? CostBreakdown { get; set; }
 }
