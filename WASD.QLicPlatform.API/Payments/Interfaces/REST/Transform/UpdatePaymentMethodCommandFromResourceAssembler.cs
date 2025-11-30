de464c870c617d7343af3c98b22e4e968656deb6
lowmath <u202215004@upc.edu.pe>
2025-11-30 18:11:13 -0500

    feat(payment): add assembler to convert UpdatePaymentMethodResource to UpdatePaymentMethodCommand

---
+++ WASD.QLicPlatform.API/Payments/Interfaces/REST/Transform/UpdatePaymentMethodCommandFromResourceAssembler.cs
@@ -0,0 +1,16 @@
+﻿using WASD.QLicPlatform.API.Payments.Domain.Model.Commands;
+using WASD.QLicPlatform.API.Payments.Interfaces.REST.Resources;
+
+namespace WASD.QLicPlatform.API.Payments.Interfaces.REST.Transform;
+
+public static class UpdatePaymentMethodCommandFromResourceAssembler
+{
+    public static UpdatePaymentMethodCommand ToCommandFromResource(int id, UpdatePaymentMethodResource resource)
+    {
+        return new UpdatePaymentMethodCommand(
+            id,
+            resource.IsDefault
+        );
+    }
+}
+
